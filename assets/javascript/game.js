cdbcde6b372739819d196c149848da6acdc2aa87
huseyintasci <huseyintasci@yandex.com>
2019-07-22 23:27:46 -0700

    Created design read txt file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,29 +4,27 @@
 
 var info = {
     moles: "",
-    score:0,
-    counter:20,
+    score: 0,
+    counter: 20
   },
   lastIndex = -1,
   onload = function() {
     info.moles = this.document.querySelectorAll("#mole");
   };
 
-  // Mole Catch
+// Mole Catch
 
-$(".mol").on("click",function(){
-whack();
-info.score++;
-$("#score").text("Score = "+info.score);
-
-
+$(".mol").on("click", function() {
+  whack();
+  info.score++;
+  $("#score").text("Score = " + info.score);
 });
 
 //Play
 var pl;
 function play() {
   randomize();
-  pl= setTimeout(play, Math.random() * 700 + 1200);
+  pl = setTimeout(play, Math.random() * 700 + 1200);
 }
 
 //Random Moles
@@ -40,7 +38,6 @@ function randomize() {
   }
   lastIndex = index;
   info.moles[index].classList.add("active");
-   
 }
 
 // ----------------------------------------------------------------------------------------------------
@@ -49,16 +46,12 @@ function randomize() {
 
 // Start Button
 $("#btn").on("click", function() {
+  // Backgroud Sound
+  game();
 
+  // ------------------------------------------
+  //   Timer
 
-// Backgroud Sound
-game();
-
-// ------------------------------------------ 
-//   Timer
-
-
-   
   $("#time").text(info.counter);
   var timer = setInterval(goDown, 1 * 1000);
 
@@ -70,38 +63,48 @@ game();
       clearInterval(timer);
       clearTimeout(pl);
       $("#time").text("Time = 0");
-
-      
+      $("#questions").html("GAME OVER");
+      alert("GAME OVER");
     }
   }
 
   // ------------------------------------------
 
-
   // Start Moles
   play();
 
+  // Questions
+  $("<iframe>", {
+    src: "assets/questions/q1.txt",
+    name: "I1",
+    width: "267",
+    height: "118",
+    frameborder: "0"
+  }).appendTo("#questions");
+
   // Score
-$("#score").text("Score = "+info.score);
+  $("#score").text("Score = " + info.score);
 
   // After click button disable
   $("#btn").attr("disabled", true);
 });
 
 
-// ------------------------------------------ 
-//   Sounds
 
+
+//   Sounds
+// ------------------------------------------
 
 // Whack Sound
-function whack(){
-  var player= document.querySelector("#whack");
-  player.play();}
+function whack() {
+  var player = document.querySelector("#whack");
+  player.play();
+}
 
-  // Background Sound
- function game(){
-  var player= document.querySelector("#game");
-  player.play();}
+// Background Sound
+function game() {
+  var player = document.querySelector("#game");
+  player.play();
+}
 
-
-   // ------------------------------------------
+// ------------------------------------------
